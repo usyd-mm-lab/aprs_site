797b41b8fc5e34ab551aa1132255bb308a828ac0
Jichao Kan <jichao.kan@outlook.com>
2021-06-20 20:06:25 +1000

    update images

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -16,41 +16,25 @@
             @sliding-end="onSlideEnd"
         >
           <!-- Text slides with image -->
-          <b-carousel-slide
-              caption="First slide"
-              text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-              img-src="https://picsum.photos/1024/480/?image=52"
-          ></b-carousel-slide>
-
-          <!-- Slides with custom text -->
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-            <h1>Hello world!</h1>
-          </b-carousel-slide>
-
-          <!-- Slides with image only -->
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-          <!-- Slides with img slot -->
-          <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
           <b-carousel-slide>
             <template #img>
-              <img
-                  class="d-block img-fluid w-100"
-                  width="1024"
-                  height="480"
-                  src="https://picsum.photos/1024/480/?image=55"
-                  alt="image slot"
-              >
+              <img :src="require('../assets/usyd_background.jpeg')" height="480"/>
             </template>
           </b-carousel-slide>
 
-          <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-          <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-              a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-            </p>
+          <b-carousel-slide>
+            <template #img>
+              <img :src="require('../assets/confphoto.jpg')"  height="480"/>
+            </template>
           </b-carousel-slide>
+
+          <b-carousel-slide>
+            <template #img>
+              <img :src="require('../assets/dicta2017.jpg')" height="480"/>
+            </template>
+          </b-carousel-slide>
+
+
         </b-carousel>
       </b-row>
       <b-row>
@@ -108,10 +92,11 @@ export default {
 </script>
 
 <style scoped>
-p{
+p {
   text-align: left;
 }
-p.intro{
+
+p.intro {
   margin-top: 20px;
 }
 </style>
